60c476b115ba869a352284037249084dee03383a
Yeo Inbo <yeo.inbo@victech.io>
2020-04-23 18:57:06 +0900

    add --config=nonccl for x64-linux ( cut dependency for libnccl.so.2)

--- victech/ports_tensorflow/tensorflow-cc/portfile.cmake
+++ victech/ports_tensorflow/tensorflow-cc/portfile.cmake
@@ -104,7 +104,7 @@ if(CMAKE_HOST_WIN32)
     )
 else()
     vcpkg_execute_build_process(
-        COMMAND ${BAZEL} build --config=cuda --verbose_failures -c opt --python_path=${PYTHON3} --incompatible_disable_deprecated_attr_params=false --define=no_tensorflow_py_deps=true //tensorflow:libtensorflow_cc.so //tensorflow:install_headers
+        COMMAND ${BAZEL} build --config=cuda --config=nonccl --verbose_failures -c opt --python_path=${PYTHON3} --incompatible_disable_deprecated_attr_params=false --define=no_tensorflow_py_deps=true //tensorflow:libtensorflow_cc.so //tensorflow:install_headers
         WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel
         LOGNAME build-${TARGET_TRIPLET}-rel
     )
