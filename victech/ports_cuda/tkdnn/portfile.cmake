f383cb55571cfd9dd395ff205f3d07e41fbc741e
dev <dev@victech.io>
2020-08-19 17:22:14 +0900

    tensorrt name bug

--- victech/ports_cuda/tkdnn/portfile.cmake
+++ victech/ports_cuda/tkdnn/portfile.cmake
@@ -7,8 +7,7 @@ vcpkg_from_github(
     SHA512 748e3d0439837fb6c1c4e58040240b5794ba271e5f7d25bfd59e2cdc9c63dc64f2e59e03735d56a96037be8472bb502134a52eafa251d444d49e2eb70d07c1e2
     HEAD_REF master
     PATCHES
-      fix_cmakelists.patch
-      fix_arm64_link_error.patch
+      renew_cmakelist_for_vcpkg.patch
 )
 
 #make sure we don't use any integrated pre-built library nor any unnecessary CMake module
@@ -22,6 +21,16 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+#install cmake for debug
+file(COPY "${CURRENT_PACKAGES_DIR}/debug/share/${PORT}/tkDNNTargets-debug.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+# remove debug directories
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/tools")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# use victech's tkDNNConfig.cmake
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/tkDNNConfig.cmake" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+
+# install license
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
