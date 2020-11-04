528d3eebec5e24354a73dc5f944215134ed48ef6
sehee <sehee@victech.io>
2020-11-04 20:20:57 +0900

    from now on, cudnn/tensorrt just verifies already installed

--- victech/ports/cudnn/portfile.cmake
+++ victech/ports/cudnn/portfile.cmake
@@ -1,68 +1,51 @@
-include(vcpkg_common_functions)
-
-if(VCPKG_TARGET_IS_UWP OR VCPKG_TARGET_IS_OSX)
-  message(FATAL_ERROR "This port is only for Windows Desktop or Linux")
-endif()
-
-if(NOT VCPKG_TARGET_ARCHITECTURE STREQUAL "x64" AND NOT VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
-  message(FATAL_ERROR "This port is only for x64 or arm64 architectures")
-endif()
-
-vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY ONLY_DYNAMIC_CRT)
+# This package doesn't install CUDNN. It instead verifies that CUDNN is installed.
+# Other packages can depend on this package to declare a dependency on CUDNN.
+# If this package is installed, we assume that CUDNN is properly installed.
 
 #note: this port must be kept in sync with CUDA port: every time one is upgraded, the other must be too
-if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-  set(CUDNN_VERSION "8.0.4")
-  set(CUDNN_FULL_VERSION "${CUDNN_VERSION}-cuda11.0")
+set(CUDNN_REQUIRED_VERSION "8.0.0")
 
-  if(VCPKG_TARGET_IS_WINDOWS)
-    set(CUDNN_DOWNLOAD_LINK "http://race.victech.io:8082/download/vcpkg_cudnn/cudnn/windows10.x86_x64/cudnn-${CUDNN_FULL_VERSION}.tar.gz")
-    set(SHA512_CUDNN "e9552d61f6e004aebae4da6166f64242ac8553ae283a2c9fdd3b39b3741f9bf8f24b1f5e4c052b8e5ef72a107a8012c0d99105389798cddc77bf1bc0993cf589")
-    set(CUDNN_OS "windows")
-  elseif(VCPKG_TARGET_IS_LINUX)
-    set(CUDNN_DOWNLOAD_LINK "http://race.victech.io:8082/download/vcpkg_cudnn/cudnn/amd64/cudnn-${CUDNN_FULL_VERSION}.tar.gz")
-    set(SHA512_CUDNN "30f4909cdfd82f4c97d3b8577b5d71a62ec4c12254099bd4d44a6b79c1d2cc37a67d16a796885f008cb504449e0bd2fc4d277926f0569f236a4961efb1171ae0")
-    set(CUDNN_OS "linux")
+if (VCPKG_TARGET_IS_WINDOWS)
+  find_path(CUDNN_INCLUDE_DIR cudnn.h
+    HINTS ENV CUDA_PATH ENV CUDA_PATH_V11_1 ENV CUDA_PATH_V11_0 ENV CUDA_PATH_V10_2 ENV CUDA_PATH_V10_1
+    PATH_SUFFIXES cuda/include include)
+elseif (VCPKG_TARGET_IS_LINUX)
+  find_path(CUDNN_INCLUDE_DIR cudnn.h
+    HINTS "/usr/local/cuda-11.1" "/usr/local/cuda-11.0" "/usr/local/cuda-10.2" "/usr/local/cuda-10.1" "/usr/local/cuda"
+    PATH_SUFFIXES cuda/include include)
+endif()
+
+if(EXISTS "${CUDNN_INCLUDE_DIR}/cudnn.h")
+  file(READ ${CUDNN_INCLUDE_DIR}/cudnn.h CUDNN_HEADER_CONTENTS)
+  if(EXISTS "${CUDNN_INCLUDE_DIR}/cudnn_version.h")
+    file(READ "${CUDNN_INCLUDE_DIR}/cudnn_version.h" CUDNN_VERSION_H_CONTENTS)
+    string(APPEND CUDNN_HEADER_CONTENTS "${CUDNN_VERSION_H_CONTENTS}")
+    unset(CUDNN_VERSION_H_CONTENTS)
+  endif()
+    string(REGEX MATCH "define CUDNN_MAJOR * +([0-9]+)"
+                 CUDNN_VERSION_MAJOR "${CUDNN_HEADER_CONTENTS}")
+    string(REGEX REPLACE "define CUDNN_MAJOR * +([0-9]+)" "\\1"
+                 CUDNN_VERSION_MAJOR "${CUDNN_VERSION_MAJOR}")
+    string(REGEX MATCH "define CUDNN_MINOR * +([0-9]+)"
+                 CUDNN_VERSION_MINOR "${CUDNN_HEADER_CONTENTS}")
+    string(REGEX REPLACE "define CUDNN_MINOR * +([0-9]+)" "\\1"
+                 CUDNN_VERSION_MINOR "${CUDNN_VERSION_MINOR}")
+    string(REGEX MATCH "define CUDNN_PATCHLEVEL * +([0-9]+)"
+                 CUDNN_VERSION_PATCH "${CUDNN_HEADER_CONTENTS}")
+    string(REGEX REPLACE "define CUDNN_PATCHLEVEL * +([0-9]+)" "\\1"
+                 CUDNN_VERSION_PATCH "${CUDNN_VERSION_PATCH}")
+  if(NOT CUDNN_VERSION_MAJOR)
+    set(CUDNN_VERSION "?")
+  else()
+    set(CUDNN_VERSION "${CUDNN_VERSION_MAJOR}.${CUDNN_VERSION_MINOR}.${CUDNN_VERSION_PATCH}")
   endif()
 else()
-  # assume jetpack 4.4
-  set(CUDNN_VERSION "8.0.0")
-  set(CUDNN_FULL_VERSION "${CUDNN_VERSION}-cuda10.2")
-
-  set(CUDNN_DOWNLOAD_LINK "http://race.victech.io:8082/download/vcpkg_cudnn/cudnn/arm64/cudnn-${CUDNN_FULL_VERSION}.tar.gz")
-  set(SHA512_CUDNN "ca544f780e9d4ea7b0a3703d2209152827179b0138315bf75cb9cf107a9cdc82e002c4fa2e18d5caa1113b498a365bdccb0c47f60c22e512df196f5b97782918")
-  set(CUDNN_OS "linux")
+  message(FATAL_ERROR "Required CUDNN version ${CUDNN_REQUIRED_VERSION} not found")
 endif()
 
-vcpkg_download_distfile(ARCHIVE
-    URLS ${CUDNN_DOWNLOAD_LINK}
-    FILENAME "cudnn-${CUDNN_FULL_VERSION}-${VCPKG_TARGET_ARCHITECTURE}-${CUDNN_OS}.tar.gz"
-    SHA512 ${SHA512_CUDNN}
-)
-
-vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
-    # NO_REMOVE_ONE_LEVEL
-)
-
-if(VCPKG_TARGET_IS_WINDOWS)
-  FILE( GLOB H_FILES  ${SOURCE_PATH}/include/*.h )
-  FILE( GLOB LIB_FILES  ${SOURCE_PATH}/lib/x64/*.lib )
-  FILE( GLOB DLL_FILES  ${SOURCE_PATH}/bin/*.dll )
-  file(INSTALL ${H_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-  file(INSTALL ${LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-  file(INSTALL ${LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-  file(INSTALL ${DLL_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-  file(INSTALL ${DLL_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-elseif(VCPKG_TARGET_IS_LINUX)
-  FILE( GLOB H_FILES  ${SOURCE_PATH}/include/*.h )
-  FILE( GLOB LIB_FILES  ${SOURCE_PATH}/lib/* )
-  file(INSTALL ${H_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-  file(INSTALL ${LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-  file(INSTALL ${LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+message(STATUS "Found CUDNN: ${CUDNN_VERSION}")
+if (CUDNN_VERSION VERSION_LESS CUDNN_REQUIRED_VERSION)
+  message(FATAL_ERROR "Required CUDNN version ${CUDNN_REQUIRED_VERSION} not found")
 endif()
 
-file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/FindCUDNN.cmake" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
--- victech/ports/tensorrt/portfile.cmake
+++ victech/ports/tensorrt/portfile.cmake
@@ -1,74 +1,54 @@
-include(vcpkg_common_functions)
+# This package doesn't install TensorRT. It instead verifies that TensorRT is installed.
+# Other packages can depend on this package to declare a dependency on TensorRT.
+# If this package is installed, we assume that TensorRT is properly installed.
 
-if(VCPKG_TARGET_IS_UWP OR VCPKG_TARGET_IS_OSX)
-  message(FATAL_ERROR "This port is only for Windows Desktop or Linux")
+#note: this port must be kept in sync with CUDA/CUDNN port: every time one is upgraded, the other must be too
+set(TensorRT_REQUIRED_VERSION "7.1.3")
+
+if (VCPKG_TARGET_IS_WINDOWS)
+  find_path(TensorRT_INCLUDE_DIR NvInfer.h
+    HINTS ENV CUDA_PATH ENV CUDA_PATH_V11_1 ENV CUDA_PATH_V11_0 ENV CUDA_PATH_V10_2 ENV CUDA_PATH_V10_1
+    PATH_SUFFIXES cuda/include include)
+elseif (VCPKG_TARGET_IS_LINUX)
+  find_path(TensorRT_INCLUDE_DIR NvInfer.h
+    HINTS /usr/local/include /usr/include 
+    PATH_SUFFIXES cuda/include include)
 endif()
 
-if(NOT VCPKG_TARGET_ARCHITECTURE STREQUAL "x64" AND NOT VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
-  message(FATAL_ERROR "This port is only for x64 or arm64 architectures")
-endif()
+if(EXISTS "${TensorRT_INCLUDE_DIR}/NvInferVersion.h")
+  file(READ ${TensorRT_INCLUDE_DIR}/NvInferVersion.h TensorRT_VERSION_HEADER_CONTENTS)
 
-vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY ONLY_DYNAMIC_CRT)
+# Sample 
+# #define NV_TENSORRT_MAJOR 7 //!< TensorRT major version.
+# #define NV_TENSORRT_MINOR 1 //!< TensorRT minor version.
+# #define NV_TENSORRT_PATCH 3 //!< TensorRT patch version.
+# #define NV_TENSORRT_BUILD 0 //!< TensorRT build number.
 
-#note: this port must be kept in sync with CUDA,cudnn port: every time one is upgraded, the other must be too
-set(TENSORRT_VERSION "7.1.3")
-
-if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-  # original download
-  # https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/
-
-  set(TENSORRT_FULL_VERSION "${TENSORRT_VERSION}-cuda11.0")
-
-  if(VCPKG_TARGET_IS_WINDOWS)
-    set(TENSORRT_DOWNLOAD_LINK "http://race.victech.io:8082/download/vcpkg_tensorrt/tensorrt/windows10.x86_x64/tensorrt-${TENSORRT_FULL_VERSION}.win.tar.gz")
-    set(SHA512_TENSORRT "234effd23aae0db204602310356106e8c98ec807440f23544bccbf812fb44bb26344015623441a39400a4097709a480216bf0fefede397a1b35c52944f95b788")
-    set(TENSORRT_OS "windows")
-  elseif(VCPKG_TARGET_IS_LINUX)
-    set(TENSORRT_DOWNLOAD_LINK "http://race.victech.io:8082/download/vcpkg_tensorrt/tensorrt/amd64/tensorrt-${TENSORRT_FULL_VERSION}.amd64.tar.gz")
-    set(SHA512_TENSORRT "8b0967d3ef91a883f564cc0f99b4b961fc2da7959a3fe7ed91d09e896fa3ec26fabe69ec8782a4996d53c9fcd531dab4033fdccc48a5c62f4941c0c73b340523")
-    set(TENSORRT_OS "linux")
+  string(REGEX MATCH "define NV_TENSORRT_MAJOR * +([0-9]+)"
+               TensorRT_VERSION_MAJOR "${TensorRT_VERSION_HEADER_CONTENTS}")
+  string(REGEX REPLACE "define NV_TENSORRT_MAJOR * +([0-9]+)" "\\1"
+               TensorRT_VERSION_MAJOR "${TensorRT_VERSION_MAJOR}")
+  string(REGEX MATCH "define NV_TENSORRT_MINOR * +([0-9]+)"
+               TensorRT_VERSION_MINOR "${TensorRT_VERSION_HEADER_CONTENTS}")
+  string(REGEX REPLACE "define NV_TENSORRT_MINOR * +([0-9]+)" "\\1"
+               TensorRT_VERSION_MINOR "${TensorRT_VERSION_MINOR}")
+  string(REGEX MATCH "define NV_TENSORRT_PATCH * +([0-9]+)"
+               TensorRT_VERSION_PATCH "${TensorRT_VERSION_HEADER_CONTENTS}")
+  string(REGEX REPLACE "define NV_TENSORRT_PATCH * +([0-9]+)" "\\1"
+                 TensorRT_VERSION_PATCH "${TensorRT_VERSION_PATCH}")
+  if(NOT TensorRT_VERSION_MAJOR)
+    set(TensorRT_VERSION "?")
+  else()
+    set(TensorRT_VERSION "${TensorRT_VERSION_MAJOR}.${TensorRT_VERSION_MINOR}.${TensorRT_VERSION_PATCH}")
   endif()
 else()
-  set(TENSORRT_FULL_VERSION "${TENSORRT_VERSION}-cuda10.2") # for cuda of jetpack 4.4
-
-  if(VCPKG_TARGET_IS_WINDOWS)
-    message(FATAL_ERROR "This port is only for Linux")
-  elseif(VCPKG_TARGET_IS_LINUX)
-    set(TENSORRT_DOWNLOAD_LINK "http://race.victech.io:8082/download/vcpkg_tensorrt/tensorrt/arm64/tensorrt-${TENSORRT_FULL_VERSION}.arm64.tar.gz")
-    set(SHA512_TENSORRT "7616dd6ecb915755a03de989527db518f34efa8bfc6a83fbda716114fa0f89b6012196c1c51883a806aec126e5f0896fc2b7f8ba2d0ad874a7a488060999dd8f")
-    set(TENSORRT_OS "linux")
-  endif()
+  message(FATAL_ERROR "Required TensorRT version ${TensorRT_REQUIRED_VERSION} not found")
 endif()
 
-vcpkg_download_distfile(ARCHIVE
-  URLS ${TENSORRT_DOWNLOAD_LINK}
-  FILENAME "tensorrt-${TENSORRT_FULL_VERSION}-${VCPKG_TARGET_ARCHITECTURE}-${TENSORRT_OS}.tar.gz"
-  SHA512 ${SHA512_TENSORRT}
-)
 
-vcpkg_extract_source_archive_ex(
-  OUT_SOURCE_PATH SOURCE_PATH
-  ARCHIVE ${ARCHIVE}
-  # NO_REMOVE_ONE_LEVEL
-)
-
-if(VCPKG_TARGET_IS_WINDOWS)
-  file(GLOB LIB_FILES "${SOURCE_PATH}/lib/*.lib")
-  file(GLOB DLL_FILES "${SOURCE_PATH}/lib/*.dll")
-  file(GLOB INCLUDE_FILES "${SOURCE_PATH}/include/*.h")
-  file(INSTALL ${LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-  file(INSTALL ${DLL_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-  file(INSTALL ${LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-  file(INSTALL ${DLL_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-  file(INSTALL ${INCLUDE_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-elseif(VCPKG_TARGET_IS_LINUX)
-  file(GLOB LIB_FILES "${SOURCE_PATH}/lib/*")
-  file(GLOB INCLUDE_FILES "${SOURCE_PATH}/include/*")
-  file(INSTALL ${LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-  file(INSTALL ${LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-  file(INSTALL ${INCLUDE_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+message(STATUS "Found TensorRT: ${TensorRT_VERSION}")
+if (TensorRT_VERSION VERSION_LESS TensorRT_REQUIRED_VERSION)
+  message(FATAL_ERROR "Required TensorRT version ${TensorRT_REQUIRED_VERSION} not found")
 endif()
 
-file(INSTALL "${SOURCE_PATH}/copyright" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/FindTensorRT.cmake" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
