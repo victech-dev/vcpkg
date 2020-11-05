8c0f6dd82d102959438fdb6c74834fd665bd455a
sehee <sehee@victech.io>
2020-11-05 15:36:33 +0900

    fix TensorflowConfig.cmake

--- victech/ports/tensorflow/TensorflowConfig.cmake
+++ victech/ports/tensorflow/TensorflowConfig.cmake
@@ -1,16 +1,16 @@
 set(TensorFlow_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../include")
 
 if(CMAKE_HOST_WIN32)
-	add_library( Tensorflow::Tensorflow_Framework SHARED IMPORTED )
-	set_target_properties( Tensorflow::Tensorflow_Framework PROPERTIES
-		IMPORTED_LOCATION                 ${CMAKE_CURRENT_LIST_DIR}/../../lib/tensorflow_framework.dll
+	add_library( TensorFlow::TensorFlow_Framework SHARED IMPORTED )
+	set_target_properties( TensorFlow::TensorFlow_Framework PROPERTIES
+		IMPORTED_LOCATION                 ${CMAKE_CURRENT_LIST_DIR}/../../bin/tensorflow_framework.dll
 		IMPORTED_IMPLIB                   ${CMAKE_CURRENT_LIST_DIR}/../../lib/tensorflow_framework.lib
 		INTERFACE_INCLUDE_DIRECTORIES     "${TensorFlow_INCLUDE_DIRS}"
 		IMPORTED_LINK_INTERFACE_LANGUAGES "C" )
 
-	add_library( Tensorflow::Tensorflow SHARED IMPORTED )
-	set_target_properties( Tensorflow::Tensorflow PROPERTIES
-		IMPORTED_LOCATION                 ${CMAKE_CURRENT_LIST_DIR}/../../lib/tensorflow.dll
+	add_library( TensorFlow::TensorFlow SHARED IMPORTED )
+	set_target_properties( TensorFlow::TensorFlow PROPERTIES
+		IMPORTED_LOCATION                 ${CMAKE_CURRENT_LIST_DIR}/../../bin/tensorflow.dll
 		IMPORTED_IMPLIB                   ${CMAKE_CURRENT_LIST_DIR}/../../lib/tensorflow.lib
 		INTERFACE_INCLUDE_DIRECTORIES     "${TensorFlow_INCLUDE_DIRS}"
 		IMPORTED_LINK_INTERFACE_LANGUAGES "C" )
@@ -18,15 +18,15 @@ if(CMAKE_HOST_WIN32)
 	set(TensorFlow_FOUND TRUE)
 	set(TensorFlow_Framework_FOUND TRUE)
 else()
-	add_library(Tensorflow::Tensorflow_Framework SHARED IMPORTED)
-	set_target_properties(Tensorflow::Tensorflow_Framework 
+	add_library(TensorFlow::TensorFlow_Framework SHARED IMPORTED)
+	set_target_properties(TensorFlow::TensorFlow_Framework 
 		PROPERTIES
 		IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/../../lib/libtensorflow_framework.so.2.3.1
 		INTERFACE_INCLUDE_DIRECTORIES "${TensorFlow_INCLUDE_DIRS}"
 	)
 	
-	add_library(Tensorflow::Tensorflow SHARED IMPORTED)
-	set_target_properties(Tensorflow::Tensorflow
+	add_library(TensorFlow::TensorFlow SHARED IMPORTED)
+	set_target_properties(TensorFlow::TensorFlow
 		PROPERTIES 
 		IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/../../lib/libtensorflow.so.2.3.1
 		INTERFACE_INCLUDE_DIRECTORIES "${TensorFlow_INCLUDE_DIRS}"
