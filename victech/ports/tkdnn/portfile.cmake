7e5fada521a097ec35fa4436d8bc495c45343f68
dev <dev@victech.io>
2020-11-08 01:09:05 +0900

    fix tkdnn window build

--- victech/ports/tkdnn/portfile.cmake
+++ victech/ports/tkdnn/portfile.cmake
@@ -9,14 +9,27 @@ vcpkg_from_github(
     PATCHES
       renew_cmakelist_for_vcpkg.patch
       clean_up_include.patch
+      fix-window-build.patch
 )
 
 #make sure we don't use any integrated pre-built library nor any unnecessary CMake module
 file(REMOVE ${SOURCE_PATH}/cmake/FindCUDNN.cmake)
 
+if (NOT VCPKG_CMAKE_SYSTEM_NAME AND NOT ENV{CUDACXX})
+  #CMake looks for nvcc only in PATH and CUDACXX env vars for the Ninja generator. Since we filter path on vcpkg and CUDACXX env var is not set by CUDA installer on Windows, CMake cannot find CUDA when using Ninja generator, so we need to manually enlight it if necessary (https://gitlab.kitware.com/cmake/cmake/issues/19173). Otherwise we could just disable Ninja and use MSBuild, but unfortunately CUDA installer does not integrate with some distributions of MSBuild (like the ones inside Build Tools), making CUDA unavailable otherwise in those cases, which we want to avoid
+  set(ENV{CUDACXX} "$ENV{CUDA_PATH}/bin/nvcc.exe")
+endif()
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+  set(BUILD_SHARED_LIBS ON)
+else()
+  set(BUILD_SHARED_LIBS OFF)
+endif()
+
 vcpkg_configure_cmake(
   SOURCE_PATH ${SOURCE_PATH}
   DISABLE_PARALLEL_CONFIGURE
+  OPTIONS -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
   PREFER_NINJA
 )
 
@@ -29,6 +42,15 @@ file(COPY "${CURRENT_PACKAGES_DIR}/debug/share/${PORT}/tkDNNTargets-debug.cmake"
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/tools")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+# enable/disable DLL header
+file(READ ${CURRENT_PACKAGES_DIR}/include/tkDNN/dll.h DLL_H)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    string(REPLACE "#ifdef tkDNN_DLL" "#if 1" DLL_H "${DLL_H}")
+else()
+    string(REPLACE "#ifdef tkDNN_DLL" "#if 0" DLL_H "${DLL_H}")
+endif()
+file(WRITE ${CURRENT_PACKAGES_DIR}/include/tkDNN/dll.h "${DLL_H}")
+
 
 # use victech's tkDNNConfig.cmake
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/tkDNNConfig.cmake" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
